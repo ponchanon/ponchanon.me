b1f8c04ddf1ebeee913c9e02b052fc0167c54a19
Ponchanon Datta Rone <ponchanon.rone@gmail.com>
2023-12-23 08:47:14 +0000

    Set Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Ponchanon Datta Rone",
+  title: "Software Engineer",
+  email: "ponchanon.rone@gmail.com",
+  gitHub: "ponchanon",
+  instagram: "ponchanon",
+  linkedIn: "ponchanon",
+  medium: "ponchanon",
+  twitter: "ponchanon",
+  youTube: "ponchanon",
 };
 
 const primaryColor = "#4E567E";
